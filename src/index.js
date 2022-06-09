2b1ed2a308f6e32056514e520637c6a855e4ee27
GlobalAssistant <globalassitant0909@gmail.com>
2022-06-09 21:21:48 +0300

    nested routes and shop preview

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import { UserProvider } from './context/user.context';
-import { ProductsProvider } from './context/products.context';
+import { CategoiresProvider } from './context/categories.context';
 import { CartProvider } from './context/cart.context';
 
 import reportWebVitals from './reportWebVitals';
@@ -17,11 +17,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoiresProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoiresProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -19,7 +19,11 @@ import {
     getFirestore,
     doc,
     getDoc,
-    setDoc
+    setDoc,
+    collection,
+    writeBatch,
+    query,
+    getDocs,
 } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
@@ -49,6 +53,35 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 
 // Get FireStore DB
 export const db = getFirestore();
+
+// Create a firestore collection
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object) => {
+        const docRef = doc(collectionRef, object.title.toLowerCase());
+        batch.set(docRef, object);
+    });
+
+    await batch.commit();
+    console.log('done');
+}
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+        const {title, items} = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {});
+
+    return categoryMap;
+}
+
 export const createUserDocumentsFromAuth = async(userAuth, additionalInformation = {}) => {
     if (!userAuth) return;
 
