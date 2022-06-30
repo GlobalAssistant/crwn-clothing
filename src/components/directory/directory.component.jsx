7a14eec976d234c1246a0d3960dc2b616531eb1b
GlobalAssistant <globalassitant0909@gmail.com>
2022-06-30 17:47:01 +0300

    styled-component: dirctory, form-input, cart-icon, home navigation

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,13 +1,49 @@
 import DirectoryItem from "../directory-item/directory-item.component"
 
-import './directory.styles.scss';
-const Directory = ({categories}) => {
+import { DirectoryContainer } from './directory.styles';
+
+const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      imageUrl: 'http://i.ibb.co/cvpntL1/hats.png',
+      route: 'shop/hats'
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageUrl: 'http://i.ibb.co/px2tCc3/jackets.png',
+      route: 'shop/jackets'
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      imageUrl: 'http://i.ibb.co/0jqHpnp/sneakers.png',
+      route: 'shop/sneakers'
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      imageUrl: 'http://i.ibb.co/GCCdy8t/womens.png',
+      route: 'shop/womens'
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      imageUrl: 'http://i.ibb.co/R70vBrQ/men.png',
+      route: 'shop/mens'
+    }
+]
+
+
+const Directory = () => {
+    
     return (
-        <div className="directory-container">
+        <DirectoryContainer>
             {categories.map((category) => (
                 <DirectoryItem key={ category.id } category={ category } />
             ))}
-        </div>
+        </DirectoryContainer>
     )
 }
 
